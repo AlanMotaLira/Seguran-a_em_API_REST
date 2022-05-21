bdb9f27993a177a1cc16a9ddaf0b0f742d5552ee
Alan Lira <alan.lira08@gmail.com>
2022-05-21 14:38:21 -0300

    feature: finalizando estrategia local

--- api/src/validation/authenticationStrategy.js
+++ api/src/validation/authenticationStrategy.js
@@ -1,22 +1,24 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const UserModels = require('../models');
-const commonValidations = require('./commonValidations');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const UserModels = require("../models");
+const commonValidations = require("./commonValidations");
 
 passport.use(
   new LocalStrategy(
     {
-      usernameField: 'email',
-      passwordField: 'password',
+      usernameField: "email",
+      passwordField: "password",
       session: false,
     },
     async (email, password, done) => {
       try {
         const user = await UserModels.searchByEmail(email);
         commonValidations.verifyUse(user);
+        commonValidations.verifyUse(password, user.password);
+        done(null, user);
       } catch (err) {
         done(err);
       }
-    },
-  ),
+    }
+  )
 );
--- api/src/validation/commonValidations.js
+++ api/src/validation/commonValidations.js
@@ -1,14 +1,16 @@
-const { InvalidArgumentError } = require('../err');
+const { InvalidArgumentError } = require("../err");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   fieldStringNotNull: (valor, name) => {
-    if (typeof valor !== 'string' || valor === 0) throw new InvalidArgumentError(`É necessário preencher o campo ${name}!`);
+    if (typeof valor !== "string" || valor === 0)
+      throw new InvalidArgumentError(`É necessário preencher o campo ${name}!`);
   },
 
   fieldSizeMinimum: (valor, name, minimo) => {
     if (valor.length < minimo) {
       throw new InvalidArgumentError(
-        `O campo ${name} precisa ser maior que ${minimo} caracteres!`,
+        `O campo ${name} precisa ser maior que ${minimo} caracteres!`
       );
     }
   },
@@ -16,13 +18,23 @@ module.exports = {
   fieldMaximumSize: (valor, name, maximo) => {
     if (valor.length > maximo) {
       throw new InvalidArgumentError(
-        `O campo ${name} precisa ser menor que ${maximo} caracteres!`,
+        `O campo ${name} precisa ser menor que ${maximo} caracteres!`
       );
     }
   },
   verifyUse(user) {
     if (!user) {
-      throw new InvalidArgumentError('Não existe usuário com o email informado');
+      throw new InvalidArgumentError(
+        "Não existe usuário com o email informado"
+      );
+    }
+  },
+  async validatePassword(password, passwordHash) {
+    const verif = await bcrypt.compare(password, passwordHash);
+    if(!verif){
+      throw new InvalidArgumentError(
+        "Email ou senha inválido"
+      );
     }
   },
 };
