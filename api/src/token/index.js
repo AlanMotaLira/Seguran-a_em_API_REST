8cbef8b15e5cffc20d3ea63f079e181f1d43a593
Alan Lira <alan.lira08@gmail.com>
2022-05-21 17:41:39 -0300

    chore: adicionado a dependencia dotenv para gerencia a senha secreta

--- api/src/token/index.js
+++ api/src/token/index.js
@@ -1,14 +1,15 @@
 const jwd = require("jsonwebtoken");
 
-
 function createTokenJWT(user) {
     const payload = {
       id: user.id,
       email:user.email
     };
   
-    const token = jwd.sign(payload, "senha-secreta");
+    const token = jwd.sign(payload, 'secretPassword');
     return token;
   }
 
   module.exports = createTokenJWT
+
+//   comando no terminal para gerar uma senha secreta interna => node -e "console.log( require('crypto').randomBytes(256).toString('base64'))"
